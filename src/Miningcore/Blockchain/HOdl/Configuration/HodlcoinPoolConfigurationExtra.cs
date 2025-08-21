2ad11f1de668b493b199014507e859540d0a81cf
real-empath <56172652+real-empath@users.noreply.github.com>
2025-08-21 02:27:41 -0500

    Create HodlcoinPoolConfigurationExtra.cs

---
+++ src/Miningcore/Blockchain/HOdl/Configuration/HodlcoinPoolConfigurationExtra.cs
@@ -0,0 +1,41 @@
+using Miningcore.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace Miningcore.Blockchain.Hodlcoin.Configuration;
+
+public class HodlcoinPoolConfigExtra
+{
+    public HodlcoinAddressType AddressType { get; set; } = HodlcoinAddressType.Legacy;
+
+    /// <summary>
+    /// Maximum number of tracked jobs.
+    /// Default: 12 - you should increase this value if your blockrefreshinterval is higher than 300ms
+    /// </summary>
+    public int? MaxActiveJobs { get; set; }
+
+    /// <summary>
+    /// Set to true to limit RPC commands to old Hodlcoin command set
+    /// </summary>
+    public bool? HasLegacyDaemon { get; set; }
+
+    /// <summary>
+    /// Set to true to fall back to multiple sendtoaddress RPC calls for payments
+    /// </summary>
+    public bool HasBrokenSendMany { get; set; } = false;
+
+    /// <summary>
+    /// Arbitrary string appended at end of coinbase tx
+    /// Overrides property of same name from HodlcoinTemplate
+    /// </summary>
+    public string CoinbaseTxComment { get; set; }
+
+    /// <summary>
+    /// Blocktemplate stream published via ZMQ
+    /// </summary>
+    public ZmqPubSubEndpointConfig BtStream { get; set; }
+
+    /// <summary>
+    /// Custom Arguments for getblocktemplate RPC
+    /// </summary>
+    public JToken GBTArgs { get; set; }
+}
