766231d29d87e71ad36538ecf427b73bd8ae4d76
real-empath <56172652+real-empath@users.noreply.github.com>
2025-08-24 14:05:29 -0500

    Delete src/Miningcore/Blockchain/Bitcoin/Custom/Hodlcoin/HodlcoinTemplate.cs

--- src/Miningcore/Blockchain/Bitcoin/Custom/Hodlcoin/HodlcoinTemplate.cs
+++
@@ -1,9 +0,0 @@
-using Miningcore.Blockchain.Bitcoin;
-
-namespace Miningcore.Blockchain.Bitcoin.Custom.Hodlcoin
-{
-    public class HodlcoinTemplate : BitcoinTemplate
-    {
-        public override int HeaderSize => 88;
-    }
-}
