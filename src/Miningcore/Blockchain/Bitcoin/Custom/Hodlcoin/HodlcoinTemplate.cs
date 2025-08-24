d8b1073e6b547e092facdd3d9b339491aeb19878
real-empath <56172652+real-empath@users.noreply.github.com>
2025-08-24 02:39:15 -0500

    Create HodlcoinTemplate.cs

---
+++ src/Miningcore/Blockchain/Bitcoin/Custom/Hodlcoin/HodlcoinTemplate.cs
@@ -0,0 +1,9 @@
+using Miningcore.Blockchain.Bitcoin;
+
+namespace Miningcore.Blockchain.Bitcoin.Custom.Hodlcoin
+{
+    public class HodlcoinTemplate : BitcoinTemplate
+    {
+        public override int HeaderSize => 88;
+    }
+}
